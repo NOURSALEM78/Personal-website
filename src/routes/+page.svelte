93ad75be74cd421127c945d794ca93521f9d248a
Nour <noursalm261@gmail.com>
2024-07-15 07:14:06 +0300

    edit colors , activate dark mode, and add pags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Themeswitch from "$lib/components/Themeswitch.svelte";
+import Button from "$lib/components/ui/button/button.svelte";
+</script>
+<title>
+    Nour salem
+</title>
+
+<nav class="w-[100vw] bg-secondary text-secondary-foreground py-8 px 4">
+    Nour Salem 
+</nav>
+
---
+++ src/routes/RESUME/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+    import Themeswitch from "$lib/components/Themeswitch.svelte";
+import Button from "$lib/components/ui/button/button.svelte";
+</script>
